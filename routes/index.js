1e1fbfad4ab036fc1ca6fbd4335e4cc2dcdbdd38
Aaron Zhao <yjzhao@alaska.edu>
2016-12-09 16:51:01 -0900

    Add year option

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let fs = require('fs');
   let _2016_data = JSON.parse(fs.readFileSync('data_2016.json','utf8'));
-  console.log(_2016_data);
-  res.render('index', { title: 'New Graduate Offers', data_2016: _2016_data});
+  let _2015_data = JSON.parse(fs.readFileSync('data_2015.json','utf8'));
+  
+  res.render('index', { title: 'New Graduate Offers', data_2016: _2016_data, data_2015: _2015_data });
 });
 
 

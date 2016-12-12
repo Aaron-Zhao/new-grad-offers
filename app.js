bb8347762e1449401d41391294f4bfa791221cd8
Aaron Zhao <yjzhao@alaska.edu>
2016-12-11 19:24:46 -0900

    Simplify code

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var offers = require('./routes/offers');
 
 var app = express();
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/offers', offers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

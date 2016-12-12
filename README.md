52441d534af991bfd042100ec9b27a093488bdb1
Aaron Zhao <yjzhao@alaska.edu>
2016-12-11 16:18:02 -0900

    Add more options and set up table views

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-Provide visual view of the new grad offer data sets.
+#Tech New Graduate and Internship Offers
 
-Data provided via Google Sheets by Jesse Collins.
+This project provides a simple bar chart view for tech job offers for new graduates and internships with the ability to filter on the data set.
+
+All data are processed and store in data_YEAR.json files.
+
+[Demo](https://google.com)
+
+Data provided by Jesse Collins via [Google Sheet](https://docs.google.com/spreadsheets/d/1Km9bsx0SWPDiOPMYvremSDujyS5sF0ZQvbxr5S52wSA/edit?usp=sharing)
+
+MIT License
+
--- routes/offers.js
+++ routes/offers.js
@@ -6,8 +6,9 @@ router.get('/', function(req, res, next) {
   let fs = require('fs');
   let _2016_data = JSON.parse(fs.readFileSync('data_2016.json','utf8'));
   let _2015_data = JSON.parse(fs.readFileSync('data_2015.json','utf8'));
+  let _2014_data = JSON.parse(fs.readFileSync('data_2014.json','utf8'));
   
-  res.render('index', { title: 'Tech New Graduate and Internship Offers', data_2016: _2016_data, data_2015: _2015_data });
+  res.render('index', { title: 'Tech New Graduate and Internship Offers', data_2016: _2016_data, data_2015: _2015_data, data_2014: _2014_data });
 });
 
 

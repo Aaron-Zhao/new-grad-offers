97b4599520e3038c087630ce48257b6b7d0c2559
Aaron Zhao <yjzhao@alaska.edu>
2016-12-12 19:31:38 -0900

    Move json files to public folder

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let fs = require('fs');
-  let _2016_data = JSON.parse(fs.readFileSync('data_2016.json','utf8'));
-  let _2015_data = JSON.parse(fs.readFileSync('data_2015.json','utf8'));
-  
-  res.render('index', { title: 'Tech New Graduate and Internship Offers', data_2016: _2016_data, data_2015: _2015_data });
+  res.render('index', { title: 'Tech New Graduate and Internship Offers'});
 });
 
 
